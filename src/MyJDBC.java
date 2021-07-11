a9a358acec793622f9dc42191fef87021462d35f
Weikang01 <lwkdota@qq.com>
2021-07-11 01:54:54 -0400

    After Updating Data in a MySQL Database

--- src/MyJDBC.java
+++ src/MyJDBC.java
@@ -1,30 +1,49 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MyJDBC {
+
+    static String url = "jdbc:mysql://localhost:3306/jdbc-demo";
+    static String user = "root";
+    static String password = "1231";
+    static Connection connection;
+    static Statement statement;
+
+    static void InsertInto(String tableName, String[] varNames, String[] varValues) throws SQLException
+    {
+        String varNameStr = String.join(", ", varNames);
+        String varValueStr = String.join(", ", varValues);
+
+        String sql = "insert into " + tableName +
+                " (" +String.join(", ", varNames) + ")" +
+                " values (" + String.join(", ", varValues) +")";
+        statement.executeUpdate(sql);
+    }
+
+    static void UpdateInfo(String tableName, String varName, String varValue, String condition) throws SQLException
+    {
+        String sql = "update " + tableName +
+                " set " + varName + "=" + varValue + " where " + condition;
+        statement.executeUpdate(sql);
+    }
+
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/jdbc-demo";
-        String user = "root";
-        String password = "1231";
-        
+
         try
         {
             // 1. Get a connection to database
-            Connection connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
 
             // 2. Create a statement
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
 
             // 3. Execute SQL query
-            String sql = "insert into employees " +
-                    " (last_name, first_name, e-mail)" +
-                    " values ('Brown', 'David', 'david.brown@foo.com')";
+            UpdateInfo("employees", "email", "'demo@luv2code.com'", "id=1");
+//            statement.executeUpdate(sql);
 
-            statement.executeQuery(sql);
-
-            System.out.println("Insert Complete.");
+//            System.out.println("Insert Complete.");
 
         }catch (Exception e)
         {
